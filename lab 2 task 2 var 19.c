81769f44c735483a203a62761f87326dff2a9910
githuuuu <92420326+githuuuu@users.noreply.github.com>
2022-09-29 15:49:04 +0300

    Update lab 2 task 2 var 19.c

--- lab 2 task 2 var 19.c
+++ lab 2 task 2 var 19.c
@@ -14,11 +14,11 @@ int main(){
 }
 double task1 (double y, double x){
     double r;
-    if(y+x>=-1 && x<0 && y<=0){
+    if(y+x>=-1 && x<0 && x>=-1 && y<=0 && y>=-1){
         r=exp(2*x)+y;
         return r;
     }
-    if(pow(x,2)+pow(y,2)<=1 && x>=0){
+    if(pow(x,2)+pow(y,2)<=1 && x>=0 && x<=1 && y>=-1 && y<=1){
         r=exp(2*x)+y;
         return r;
     }
