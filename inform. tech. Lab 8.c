910edd3ca2f8d3a20c428a52265e5c3b3254d65f
githuuuu <92420326+githuuuu@users.noreply.github.com>
2022-11-07 10:44:11 +0300

    Create inform. tech. Lab 8.c

---
+++ inform. tech. Lab 8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+float a=3, b=1, c=1, a1=2, b1=-5, c1=-22, x ,y;
+y=((a*c1)-(a1*c))/((a*b1)-(a1*b));
+x=(c-(b*y))/a;
+printf("X= %f\tY= %f",x,y);
+}
